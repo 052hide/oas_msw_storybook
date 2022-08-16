4c3bfb4b39fbe09f8ccbd795472aac0378ec9ca6
052hide <hidetoshi1990@gmail.com>
2022-08-16 15:50:28 +0900

    add mock handlers

---
+++ .mocks/handlers.ts
@@ -0,0 +1,43 @@
+import { rest } from 'msw'
+import schema from '../openapi.json'
+
+export const HANDLERS = {
+  fetchPets: {
+    success: (
+      example: keyof typeof schema.paths['/pets']['get']['responses']['200']['content']['application/json']['examples']
+    ) => {
+      rest.get('', (_req, res, ctx) => {
+        return res(
+          ctx.delay(1000),
+          ctx.status(200),
+          ctx.json(schema.paths['/pets'].get.responses[200].content['application/json'].examples[example]),
+        )
+      })
+    },
+  },
+  addPet: {
+    success: (
+      example: keyof typeof schema.paths['/pets']['post']['responses']['200']['content']['application/json']['examples']
+    ) => {
+      rest.get('', (_req, res, ctx) => {
+        return res(
+          ctx.delay(1000),
+          ctx.status(200),
+          ctx.json(schema.paths['/pets'].post.responses[200].content['application/json'].examples[example]),
+        )
+      })
+    },
+    invalid: (
+        example: keyof typeof schema.paths['/pets']['post']['responses']['400']['content']['application/json']['examples']
+      ) => {
+        rest.get('', (_req, res, ctx) => {
+          return res(
+            ctx.delay(1000),
+            ctx.status(400),
+            ctx.json(schema.paths['/pets'].post.responses[400].content['application/json'].examples[example]),
+          )
+        })
+      },
+  }
+}
+
